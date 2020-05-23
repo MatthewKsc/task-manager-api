f6b2e151f84ae0698860e444d7752de0e6b754ec
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-23 20:17:41 +0200

    Added TaskService and TaskController

---
+++ task-manager/src/main/java/com/matthewksc/taskmanager/controllers/TaskController.java
@@ -0,0 +1,40 @@
+package com.matthewksc.taskmanager.controllers;
+
+import com.matthewksc.taskmanager.dao.Task;
+import com.matthewksc.taskmanager.services.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/all")
+    public Iterable<Task> getAllTasks(){
+        return taskService.findAll();
+    }
+
+    @GetMapping("/{Id}")
+    public Optional<Task> getTaskById(@PathVariable Long Id){
+        return taskService.findById(Id);
+    }
+
+    @PostMapping
+    public Task saveTask(@RequestBody Task task){
+        taskService.save(task);
+        return task;
+    }
+
+    @DeleteMapping("/{Id}")
+    public void deleteTaskById(@PathVariable Long Id){
+        taskService.deleteById(Id);
+    }
+
+}
--- task-manager/src/main/java/com/matthewksc/taskmanager/services/TaskService.java
+++ task-manager/src/main/java/com/matthewksc/taskmanager/services/TaskService.java
@@ -1,4 +1,33 @@
 package com.matthewksc.taskmanager.services;
 
+import com.matthewksc.taskmanager.dao.Task;
+import com.matthewksc.taskmanager.dao.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Optional<Task> findById(Long Id) {
+        return taskRepository.findById(Id);
+    }
+
+    public Iterable<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public void deleteById(Long Id) {
+        taskRepository.deleteById(Id);
+    }
 }
