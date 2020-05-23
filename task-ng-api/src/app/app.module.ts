6e69abfe7246c7ab61bbd3f7b5427bc3c2c35aee
MatthewKsc <ksciukmateusz@gmail.com>
2020-05-23 21:42:17 +0200

    Added 3 modules and imported routers, httpClient, formModule

--- task-ng-api/src/app/app.module.ts
+++ task-ng-api/src/app/app.module.ts
@@ -2,13 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes: Routes =[
+  {
+    path:'task',
+    component: TaskComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch:'full'
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
