c73d9658f6df938eeb83342a88204057e9c31e65
MatthewKsc <ksciukmateusz@gmail.com>
2020-06-01 18:26:27 +0200

    TaskListControllerTest impl

--- task-manager/src/test/java/com/matthewksc/taskmanager/controllers/TaskListControllerTest.java
+++ task-manager/src/test/java/com/matthewksc/taskmanager/controllers/TaskListControllerTest.java
@@ -1,27 +1,92 @@
 package com.matthewksc.taskmanager.controllers;
 
+import com.matthewksc.taskmanager.dao.entity.TaskList;
+import com.matthewksc.taskmanager.services.TaskListService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = TaskListController.class)
+@WebMvcTest(controllers = TaskListController.class,
+        //disable our WebSecurityConfig
+        useDefaultFilters = false,
+        //we Adding specific filter to be use
+        includeFilters = {@ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                value = TaskListController.class
+        )}) //this include spring security to our test's
+//this disable spring security filter to test endpoints (disable JWT)
+@AutoConfigureMockMvc(addFilters = false)
 class TaskListControllerTest {
 
+    private static String BASIC_URL = "/tasks/lists";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskListService taskListService;
+
     @Test
-    void getAllLists() {
+    void getAllLists() throws Exception {
+        given(taskListService.findAll()).willReturn(Arrays.asList(
+                new TaskList(),
+                new TaskList()
+        ));
+        mockMvc.perform(get(BASIC_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+        verify(taskListService, times(1)).findAll();
     }
 
     @Test
-    void getListById() {
+    void getListById() throws Exception {
+        TaskList taskList = new TaskList();
+        taskList.setCategoryOfTask("test");
+        given(taskListService.findById(1L)).willReturn(taskList);
+
+        mockMvc.perform(get(BASIC_URL+"/"+1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(taskList.getId())))
+                .andExpect(jsonPath("$.tasks", is(taskList.getTasks())))
+                .andExpect(jsonPath("$.categoryOfTask", is(taskList.getCategoryOfTask())));
+        verify(taskListService, times(1)).findById(1L);
     }
 
     @Test
-    void saveTaskList() {
+    void saveTaskList() throws Exception{
+        mockMvc.perform(post(BASIC_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void deleteListById() {
+    void deleteListById() throws Exception{
+        mockMvc.perform(delete(BASIC_URL+"/"+1))
+                .andExpect(status().isOk());
+
+        verify(taskListService, times(1)).deleteById(1L);
     }
 }
